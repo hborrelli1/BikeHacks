d651068a003cba53c1aece9015b75f4f40fd34aa
Harry Borrelli <hborrelli@Harrys-MacBook-Pro-2.local>
2021-08-30 21:54:14 -0600

    Adding to bike hacks data.

--- server/hacks.js
+++ server/hacks.js
@@ -1,7 +1,9 @@
 [
   {
-    "title": "Setup Tubeless Tires",
+    "title": "How to set up tubeless tires",
     "id": 0,
+    "url": "how-to-set-up-tubless-tires",
+    "estimated-time": "15 minutes",
     "steps": [
       {
         "title": "Step 1.",
@@ -15,41 +17,114 @@
       },
       {
         "title": "Step 3.",
+        "description": "Use a small phillips head screwdriver to poke a hole where the valve stem will be inserted. Be careful not to make the hole too big."
+      },
+      {
+        "title": "Step 4.",
+        "description": "Insert the tubeless valve stem into the valve hole and screw it tight.",
+        "notes": "Make sure the O-ring and washer are set properly."
+      },
+      {
+        "title": "Step 5.",
         "description": "Place one side of the new tubeless-ready tires around the rim",
         "notes": "Tubeless tires have a special bead that seals on to the rim."
       },
       {
-        "title": "Step 4.",
-        "description": "Pour about 3 liquid ounces of tire seal into the tire.",
+        "title": "Step 6.",
+        "description": "Pour recommended amount of tire seal into the tire.",
         "notes": "Tube seal can be purchased online or from most bike shops. Tube seal will fill any cracks or holes and self-seal."
+      },
+      {
+        "title": "Step 7.",
+        "description": "Put the tire completely on the rim. Using a valve core remover tool, remove the valve core from your tubelss valve and inject the recommended amount of sealant through the valve."
+      },
+      {
+        "title": "Step 8.",
+        "description": "Spin the tire around a few times to disperse the sealant.",
+        "notes": "You may need to pull the bead of the tire to the edge of the rim, if possible."
+      },
+      {
+        "title": "Step 9.",
+        "description": "Inflate the tire. The best way to get a proper seal is by using an air compressor."
       }
     ],
     "tools": [
+      "Tubeless-compatible tires and rims",
+      "Tubeless sealant",
+      "Rim tape",
+      "Tubeless valve",
       "Tire lever",
       "Air compressor or high pressure bike pump",
-      "Tire sealant",
       "Tubeless ready tires"
     ]
   },
   {
-    "title": "Bleed Brakes",
+    "title": "How to bleed Shimano disc brakes",
     "id": 1,
+    "url": "how-to-bleed-hydrolic-brakes",
+    "estimated-time": "30 minutes",
     "steps": [
       {
         "title": "Step 1.",
-        "description": "Locate the break bleeding screw on your brakes.",
-        "notes": "The bleed screws are often found near the brake lever."
+        "description": "Remove the rear wheel and brake pads. The Shimano brake pads will b esecured by a cotter pin or bolt with the 3 mm allen wrench."
       },
       {
         "title": "Step 2.",
-        "description": "Remove the bleed screw from the bottom of the brake. Be sure to have a container ready to dump old brake fluid in to discard.",
-        "notes": "The bottom bleed screw can be found near the disc brake."
+        "description": "Install the Bleed Block using the cotter pin or bolt."
+      },
+      {
+        "title": "Step 3.",
+        "description": "Adjust the brake level so it is level. This helps air bubbles rise to the top."
+      },
+      {
+        "title": "Step 4.",
+        "description": "Remove the bleed fitting screw with the 2.5 mm allen wrench and thread in the Shimano brake bleed funnel and leave the plunger in."
+      },
+      {
+        "title": "Step 5.",
+        "description": "Fill the syringe with Shimano hydraulic mineral oil and place the syringe on the bleed nipple."
+      },
+      {
+        "title": "Step 6.",
+        "description": "Open the bleed fitting using the 7 mm box wrench."
+      },
+      {
+        "title": "Step 7.",
+        "description": "Remove the pluginer from the Shimano bleed funnel and begin pushing fluid through the system. CLose off the bleed fitting and remove the syringe. Add a bit of suction ot the syringe to avoid splashing any hydrolic fluid on your brakes."
+      },
+      {
+        "title": "Step 8.",
+        "description": "Remove remaining air bubbles by alternating between full pulls and light flicks on the brake lever. Replace the pluger and remove the funnel."
+      },
+      {
+        "title": "Step 9.",
+        "description": "Replace the plunger and remove the funnel. Replace the bleed fitting screws or caps at both the lever and the caliper sides."
+      },
+      {
+        "title": "Step 10.",
+        "description": "Clean brake levers and caliper with isopropyl alcohol using a rag. Remove the Shimano brake bleed block and re-install brake pads."
+      },
+      {
+        "title": "Step 11.",
+        "description": "Re-install rear wheel.",
+        "notes": "Repeat the process for the front wheel if needed."
+      },
+      {
+        "title": "Step 12.",
+        "description": "Go shred!"
       }
     ],
     "tools": [
-      "Brake fluid",
-      "Brake bleed syringe",
-      "Container to dump old brake fluid in."
+      "Syringe",
+      "Shimano Hydraulic Mineral Oil",
+      "Isopropyl Alcohol",
+      "Shimano Bleed Funnel",
+      "2.5 mm Allen Wrench",
+      "3 mm Allen Wrench or Pliers, depending on brake style.",
+      "4 mm Allen Wrench",
+      "5 mm Allen Wrench",
+      "7 mm Box Wrench",
+      "Bleed Block"
     ]
   }
 ]
