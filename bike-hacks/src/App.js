d3f819d9c79da5c6ee3c9508684309c1a13105fd
Harry Borrelli <hborrelli@Harrys-MacBook-Pro-2.local>
2021-08-30 18:06:24 -0600

    Initialize react app with minimal functionality

---
+++ bike-hacks/src/App.js
@@ -0,0 +1,14 @@
+import Header from './Components/Header';
+import TutorailsList from './Components/TutorialsList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <TutorailsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ bike-hacks/src/Components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Bike Hacks</h1>
+    </header>
+  );
+}
+ 
+export default Header;
