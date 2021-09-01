7a025508fd3b0935e0e2a03dee3b0267d5ee03d6
Harry Borrelli <hborrelli@Harrys-MacBook-Pro-2.local>
2021-08-31 22:28:32 -0600

    Added tutorials.

--- server/hacks.js
+++ server/hacks.js
@@ -4,47 +4,40 @@
     "id": 0,
     "url": "how-to-set-up-tubless-tires",
     "estimated-time": "15 minutes",
+    "iconPath": "/assets/icons/wheel-icon.png",
     "steps": [
       {
         "title": "Step 1.",
-        "description": "If you haven't already, remove the existing tires and tubes from the wheels.",
+        "description": "If you haven't already, remove the existing tires and tubes from the wheels. Clean the inside of the wheel and ensure the rim tape is still good. If the rim tape is old or damaged, replace the rim tape.",
         "notes": "A tire lever can be useful to remove stubborn tires."
       },
       {
         "title": "Step 2.",
-        "description": "Clean the inside of the wheel and ensure the rim tape is still good. If the rim tape is old or damaged, replace the rim tape.",
-        "notes": "Rim tape can be purchased online or from most bike shops."
-      },
-      {
-        "title": "Step 3.",
         "description": "Use a small phillips head screwdriver to poke a hole where the valve stem will be inserted. Be careful not to make the hole too big."
       },
       {
+        "title": "Step 3.",
+        "description": "Insert the tubeless valve stem into the valve hole and screw it tight. Make sure the O-ring and washer are set properly."
+      },
+      {
         "title": "Step 4.",
-        "description": "Insert the tubeless valve stem into the valve hole and screw it tight.",
-        "notes": "Make sure the O-ring and washer are set properly."
+        "description": "Place one side of the new tubeless-ready tires around the rim. Tubeless tires have a special bead that seals on to the rim."
       },
       {
         "title": "Step 5.",
-        "description": "Place one side of the new tubeless-ready tires around the rim",
-        "notes": "Tubeless tires have a special bead that seals on to the rim."
+        "description": "Pour recommended amount of tire seal into the tire. Typically you will need about 3 fl oz of tire sealant."
       },
       {
         "title": "Step 6.",
-        "description": "Pour recommended amount of tire seal into the tire.",
-        "notes": "Tube seal can be purchased online or from most bike shops. Tube seal will fill any cracks or holes and self-seal."
-      },
-      {
-        "title": "Step 7.",
         "description": "Put the tire completely on the rim. Using a valve core remover tool, remove the valve core from your tubelss valve and inject the recommended amount of sealant through the valve."
       },
       {
-        "title": "Step 8.",
-        "description": "Spin the tire around a few times to disperse the sealant.",
+        "title": "Step 7.",
+        "description": "Spin the tire around a few times to disperse the sealant. The tire sealant will fill any holes or gaps ensuring a tight seal.",
         "notes": "You may need to pull the bead of the tire to the edge of the rim, if possible."
       },
       {
-        "title": "Step 9.",
+        "title": "Step 8.",
         "description": "Inflate the tire. The best way to get a proper seal is by using an air compressor."
       }
     ],
@@ -62,7 +55,8 @@
     "title": "How to bleed Shimano disc brakes",
     "id": 1,
     "url": "how-to-bleed-hydrolic-brakes",
-    "estimated-time": "30 minutes",
+    "estimated-time": "40 minutes",
+    "iconPath": "/assets/icons/brake-lever-icon.png",
     "steps": [
       {
         "title": "Step 1.",
@@ -125,6 +119,142 @@
       "5 mm Allen Wrench",
       "7 mm Box Wrench",
       "Bleed Block"
+    ]
+  },
+  {
+    "title": "How to adjust rear Durailleur",
+    "id": 2,
+    "url": "how-to-adjust-rear-durailleur",
+    "estimated-time": "30 minutes",
+    "iconPath": "/assets/icons/durailleur-icon.png",
+    "steps": [
+      {
+        "title": "Step 1.",
+        "description": "Begin with a visual check of the derailleur hanger. If it’s clearly bent, your adjustments are not going to work. Refer to our video on hanger alignment to fix this issue. If the bike has multiple front chainrings, shift the front derailleur to the largest chainring, then shift the rear derailleur to the smallest cog. Even if you are already in the smallest cog, keep clicking your rear shifter to make sure there are no clicks left in the mechanism. This makes sure that the rear shifter is fully actuated outward."
+      },
+      {
+        "title": "Step 2.",
+        "description": "If the chain does not shift to the smallest cog, it is likely being stopped either by the limit screw, or cable setting. First turn the H-limit screw count-clockwise a few turns. Pedal and see if it makes the shift outward. If it still does not shift outward, turn the barrel adjuster on the derailleur body clockwise a few rotations. Pedal to see if it shifts. Your chain should now be on the smallest cog and there should be no more clicks left in the shifter mechanism. Turn the barrel adjuster clockwise another two turns, or until it is screwed in all the way, whichever comes first. The reason we do this comes from the earlier point regarding shifting position and proper limit screw settings. This will move the shifting positions beyond the limit screw setting so we will get an accurate assessment of current limit screw settings. We are now ready to dial in the H-limit screw."
+      },
+      {
+        "title": "Step 3.",
+        "description": "If the limit screws are unmarked, pick one and turn it in and out while watching the derailleur. If the derailleur does not move, try the other limit screw. The H-limit screw will cause the derailleur to move. Find the correct setting by first making the H-limit screw adjustment too tight. Now back out the H limit screw until it sits directly below the smallest cog. Take your screwdriver and tighten the H-limit screw one half turn. Listen for excessive noise while pedaling. If there isn’t any excessive noise coming from the drivetrain, tighten the screw until you hear the chain rubbing on the next cog. Once you hear the excessive noise, begin to back out the H-limit screw one quarter turn at a time until the noise stops. If there are two settings that seem equally quiet, go with the tighter of the two settings. The H-limit is now set.",
+        "notes": "The idea is to bring it back to roughly where it was before. Don’t worry if it isn’t completely precise just yet. As far as the L-limit screw, we will wait to adjust that until after we have adjusted the derailleur indexing and cable tension."
+      },
+      {
+        "title": "Step 4.",
+        "description": "If there are two front chainrings, stay on the largest chainring. If your bike uses three front chainrings, shift to the middle. On the rear, start on the smallest cog. Pedaling at a normal riding cadence, shift the shifter only one indexed click, not more than one click. We want this one click to shift one, and only one gear. If the chain did not make it to the next gear, return the shift lever to the outermost click, and turn the barrel adjuster one full turn counter-clockwise. Try the shift again and repeat the process until it makes the shift. If you have un-threaded the barrel adjuster that it has come out, or has nearly come out, thread the barrel back in fully and then out one or two turns. Make sure that you are on the furthest outward shift position and the smallest cog, then remove the slack from the cable at the pinch bolt.",
+        "notes": "The process of indexing is to line up the guide pulley with the cogs so that each incremental shift lines up with each cog. The barrel adjuster allows us to make these adjustments."
+      },
+      {
+        "title": "Step 5.",
+        "description": "Similar to the H screw setting, make the L screw too tight and back it out slightly until it is just right. This will provide the most protection from the derailleur shifting the chain beyond the largest cog and into the spokes. Shift the chain to the next to largest chainring in front. Shift to the second largest cog in the rear. Now, try shifting to the largest cog in order to determine where the L-limit screw is currently set. If the chain does not make the shift, than the L screw is already too tight, and that’s where we want it for now. If the chain shifts slowly to the largest cog, that is also a symptom of a too tight L screw. If there is excessive noise when the chain is on the largest cog, again, the L screw is too tight. If it makes an acceptable shift with no excessive noise, the L screw is not too tight. Shift the derailleur one click outward and tighten the L screw one turn clockwise and check the shift again. Repeat this process until there are symptoms of a too tight L screw. Once the too tight L screw is causing symptoms, turn the L screw out one quarter turn until and check the shift to the largest cog until the symptoms of excessive noise and slow shifting go away. The L-limit is now set.",
+        "notes": "As a check, put extra pressure on the shifter and watch at the rear derailleur. The cage should not move inward."
+      },
+      {
+        "title": "Step 6.",
+        "description": "This adjustment is check when the chain is on the smallest sprocket up front, and the largest cog in back. The majority of road and mountain bike derailleurs require a gap between 5 and 6 millimeters. You can use a hex wrench to gauge this distance. Don’t get too hung up on this particular adjustment. If the bike is shifting well, the B screw is probably within acceptable adjustment range. To increase the gap, tighten the B screw. To decrease the gap, loosen the B screw. If you do make significant changes to the B screw, double check the indexing adjustment. Otherwise you are done, and your derailleur is properly adjusted.",
+        "notes": "Most derailleurs have some form of adjustment for the spacing of the guide pulley to the cogs. This spacing is controlled by the B screw, or body-angle screw."
+      }
+    ],
+    "tools": [
+      "Repair Stand",
+      "Screwdriver - Phillips #2",
+      "Hex Wrenches",
+      "Forth Hand Tool",
+      "Cable Cutter, if trimming cable",
+      "Light lubricant such as CL-1",
+      "Rags"
+    ]
+  },
+  {
+    "title": "How to replace a bicycle chain",
+    "id": 3,
+    "url": "how-to-replace-bicycle-chain",
+    "estimated-time": "20 minutes",
+    "iconPath": "/assets/icons/chain-icon.png",
+    "steps": [
+      {
+        "title": "Step 1.",
+        "description": "First, jettison the old chain. If it has a quick link (a two-piece link that can be squeezed together to unfasten it), you can compress this to split the chain. We’re working on a Shimano chain – like most brands, you’ll need to use a chain tool to push one pin out to split the chain. Slot the links between the teeth of the tool and give it a twist until the pin pops out.",
+        "notes": "A quick link is a section of the chain that allows you pinch together with a chain tool to remove the chain."
+      },
+      {
+        "title": "Step 2.",
+        "description": "Now is a good time to clean and inspect your chainrings and cassette. Check that the profiles of all the teeth are symmetrical. Once excessively worn, they begin to take on a profile like a shark’s tooth as the chain eats into them. Assuming everything looks healthy, scrub both down with a cleaning solution – no point fitting a clean chain onto dirty cogs!"
+      },
+      {
+        "title": "Step 3.",
+        "description": "Shift down to the smallest sprocket and chainring. Thread the new chain through the front derailleur cage. Next, pull it across and loop it over the rear cassette and round the upper jockey wheel on the derailleur. Thread it through the cage, being careful to keep inside the divider between the plates, and then around the lower wheel.",
+        "notes": "Shifting to the smallest gear is not necessary, but it will help with removing the chain by reducing the amount of tension."
+      },
+      {
+        "title": "Step 4.",
+        "description": "Your new chain will need to be cut to size. With the chain around the smallest sprocket and chainring, pull the ends together so they meet below the chainstay. You want to find the longest possible length that still generates some tension on the rear derailleur so that it won’t drag. Remember, you’ll need to join an internal link to an external one."
+      },
+      {
+        "title": "Step 5.",
+        "description": "Shimano recommends that the open end faces backwards. Having noted the link that you need to sever, it’s time to grab the chain tool again. Drop the link you want to work on in between the jaws of the tool. Turn the handle until the pin starts to push the centre of the link. Be careful that everything is aligned so as not to damage the tool."
+      },
+      {
+        "title": "Step 6.",
+        "description": "Find the pointy end of the joining pin that came packaged with your new chain. Take the ends of the chain and slot them together. Facing the bike from the drive side, push the pin into the chain to hold it together. It should stay in place without you needing to hold it. Pick up the chain tool again and unscrew the handle a few turns."
+      },
+      {
+        "title": "Step 7.",
+        "description": "Slot the link held together with the pin into the chain tool. Screw the handle down to push the pin home. As the pin becomes almost flush with the outer plate of the link, it will become easier to turn the handle. Work very slowly – you want the end of the pin to be exactly flush with the outside of the plate. Once it’s there, remove the chain tool."
+      },
+      {
+        "title": "Step 8.",
+        "description": "Check the link you’ve joined moves freely. If it doesn’t and the pin is protruding more than the others in the chain, reattach the chain tool and give it a little push. If it appears to be in the correct position, try flexing the chain horizontally to free it up. Once you’re happy, snap the end off the back of the pin by giving it a sharp twist using the back of the tool or pliers.",
+        "notes": "Now is a good time to ensure the chain is linked properly and that the master link is not loose."
+      }
+    ],
+    "tools": [
+      "Repair Stand",
+      "Chain Tool",
+      "New Chain",
+      "Light lubricant such as CL-1",
+      "Rags"
+    ]
+  },
+  {
+    "title": "How to adjust your saddle",
+    "id": 4,
+    "url": "how-to-adjust-your-saddle",
+    "estimated-time": "5 minutes",
+    "iconPath": "/assets/icons/bike-saddle-icon.png",
+    "steps": [
+      {
+        "title": "Step 1.",
+        "description": "The bolts you want are normally on the underside of the seatpost. You’ll need to turn them clockwise as viewed from above the saddle. For designs with two bolts, slacken them off evenly, turning each a little at a time. Single-bolt designs usually require a 6mm Allen key while a 5mm Allen key is more common for twin-bolt set-ups."
+      },
+      {
+        "title": "Step 2.",
+        "description": "With the bolts removed the clamp head will come free. Spray from the road can make the whole assembly gritty, so give all the parts a wipe down with a rag and some degreaser. Check the inside of the post cradle for wear and look for excessive crimping or other damage to the saddle rails.",
+        "notes": "Cleaning your saddle is not necessary, but will help with the longevity of your stem and saddle."
+      },
+      {
+        "title": "Step 3.",
+        "description": "The bolts and specially shaped nuts that hold the saddle clamp together can get grimy, so give them the same treatment as the clamp parts. Clean with solvent, check for wear and then give them a dab of light oil, grease or anti-seize assembly lube before replacing."
+      },
+      {
+        "title": "Step 4.",
+        "description": "Assemble the saddle in the clamp head and place both parts onto the top of the post. Return the bolts to their former positions and tighten them by hand – but not too tight. With the bolt (or bolts) still loose, you should be able to shunt the saddle rails forwards or backwards in the cradle to the required position."
+      },
+      {
+        "title": "Step 5.",
+        "description": "Unless you’ve been convinced otherwise, it’s best to keep your saddle flat rather than tilted up or down. With single-bolt designs, just loosen the bolt and rock the cradle. With twin-bolt designs, tightening the back bolt will tilt the saddle upwards, tightening the front one, will tilt it downwards. You'll need to balance the two before doing them both up quite tightly."
+      },
+      {
+        "title": "Step 6.",
+        "description": "Next, remove the seatpost for cleaning – this will reduce the chance of it seizing in place. Give both the seatpost and the inside of the frame a quick clean, and if you’re using aluminium parts, a light coat of anti-seize.",
+        "notes": "For carbon seatposts, either leave it ungreased or use carbon assembly paste."
+      }
+    ],
+    "tools": [
+      "Bike Saddle",
+      "Hex Wrenches"
     ]
   }
 ]
