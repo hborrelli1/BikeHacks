eb90f819de81635bf5d96176f8ba5b9313d89872
Harry Borrelli <hborrelli@Harrys-MacBook-Pro-2.local>
2021-09-01 14:46:03 -0600

    Remove css property.

--- bike-hacks/src/Components/ScrollToTopOnMount.js
+++ bike-hacks/src/Components/ScrollToTopOnMount.js
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 const ScrollToTopOnMount = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
-    console.log('scrolling...');
   }, []);
   
   return null;
